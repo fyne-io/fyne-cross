1a48a5ccb1aa8dc90eb7ec4c21e332e20adcd074
lucor <lu.corbo@gmail.com>
2019-07-06 12:03:39 +0200

    Update godoc

--- doc.go
+++ doc.go
@@ -1,16 +1,22 @@
 /*
-fyne-cross is a simple tool to cross compile Fyne applications (https://fyne.io)
+fyne-cross is a simple tool to cross compile Fyne applications
 
-It uses a docker image built on top of golang-cross (https://hub.docker.com/r/dockercore/golang-cross/),
-that includes the MinGW compiler for windows, and an OSX SDK, along the Fyne requirements.
+It has been inspired by xgo and uses a docker image built on top of the
+golang-cross () image, that includes the MinGW compiler for windows, and an OSX
+SDK, along the Fyne requirements.
 
 Supported targets are:
-  -  windows/386
   -  darwin/amd64
   -  darwin/386
   -  linux/amd64
   -  linux/386
   -  windows/amd64
+  -  windows/386
 
+References
+- fyne: https://fyne.io
+- xgo: https://github.com/karalabe/xgo
+- golang-cross: https://github.com/docker/golang-cross
+- fyne-cross docker images: https://hub.docker.com/r/lucor/fyne-cross
 */
 package main
