4d4d8507aeb1598a8cff80ce0020acf2cdcc8fce
lucor <lu.corbo@gmail.com>
2019-09-07 18:27:50 +0200

    Release 1.0.0

--- build.go
+++ build.go
@@ -13,7 +13,7 @@ import (
 )
 
 const dockerImage = "lucor/fyne-cross"
-const version = "dev"
+const version = "1.0.0"
 
 // targetWithBuildOpts represents the list of supported GOOS/GOARCH with the relative
 // options to build
