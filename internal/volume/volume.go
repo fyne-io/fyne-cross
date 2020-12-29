fyne-io/fyne-cross

commit d949bc3064d7d701aed120df095fc1ebfd39db25

Fix android keystore path is not resolved correctly

internal/volume/volume.go
@@ -103,13 +103,13 @@ func Mount(workDirHost string, cacheDirHost string) (Volume, error) {
 // JoinPathContainer joins any number of path elements into a single path,
 // separating them with the Container OS specific Separator.
 func JoinPathContainer(elem ...string) string {
-	return strings.Join(elem, "/")
+	return filepath.Clean(strings.Join(elem, "/"))
 }
 
 // JoinPathHost joins any number of path elements into a single path,
 // separating them with the Host OS specific Separator.
 func JoinPathHost(elem ...string) string {
-	return filepath.Join(elem...)
+	return filepath.Clean(filepath.Join(elem...))
 }
 
 // Zip compress the source file into a zip archive

internal/command/android.go
@@ -3,6 +3,7 @@ package command
 import (
 	"fmt"
 	"os"
+	"path"
 	"path/filepath"
 
 	"github.com/fyne-io/fyne-cross/internal/log"
@@ -187,7 +188,16 @@ func makeAndroidContext(flags *androidFlags, args []string) (Context, error) {
 	ctx.OS = androidOS
 	ctx.ID = androidOS
 
-	ctx.Keystore = flags.Keystore
+	if path.IsAbs(flags.Keystore) {
+		return Context{}, fmt.Errorf("keystore location must be relative to the project root: %s", ctx.Volume.WorkDirHost())
+	}
+
+	_, err = os.Stat(volume.JoinPathHost(ctx.Volume.WorkDirHost(), flags.Keystore))
+	if err != nil {
+		return Context{}, fmt.Errorf("keystore location must be under the project root: %s", ctx.Volume.WorkDirHost())
+	}
+
+	ctx.Keystore = volume.JoinPathContainer(ctx.Volume.WorkDirContainer(), flags.Keystore)
 	ctx.KeystorePass = flags.KeystorePass
 	ctx.KeyPass = flags.KeyPass
 

internal/command/android_test.go
@@ -1,0 +1,115 @@
+package command
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_makeAndroidContext(t *testing.T) {
+	vol, err := mockDefaultVolume()
+	require.Nil(t, err)
+
+	type args struct {
+		flags *androidFlags
+		args  []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Context
+		wantErr bool
+	}{
+		{
+			name: "keystore path must be relative to project root",
+			args: args{
+				flags: &androidFlags{
+					CommonFlags: &CommonFlags{
+						AppBuild: 1,
+						AppID:    "test",
+					},
+					Keystore: "/tmp/my.keystore",
+				},
+			},
+			want:    Context{},
+			wantErr: true,
+		},
+		{
+			name: "keystore path does not exist",
+			args: args{
+				flags: &androidFlags{
+					CommonFlags: &CommonFlags{
+						AppBuild: 1,
+						AppID:    "test",
+					},
+					Keystore: "my.keystore",
+				},
+			},
+			want:    Context{},
+			wantErr: true,
+		},
+		{
+			name: "default",
+			args: args{
+				flags: &androidFlags{
+					CommonFlags: &CommonFlags{
+						AppBuild: 1,
+						AppID:    "test",
+					},
+					Keystore: "testdata/my.keystore",
+				},
+			},
+			want: Context{
+				AppBuild:     "1",
+				AppID:        "test",
+				ID:           androidOS,
+				OS:           androidOS,
+				DockerImage:  androidImage,
+				Volume:       vol,
+				CacheEnabled: true,
+				StripDebug:   true,
+				Package:      ".",
+				Keystore:     "/app/testdata/my.keystore",
+			},
+			wantErr: false,
+		},
+		{
+			name: "default",
+			args: args{
+				flags: &androidFlags{
+					CommonFlags: &CommonFlags{
+						AppBuild: 1,
+						AppID:    "test",
+					},
+					Keystore: "./testdata/my.keystore",
+				},
+			},
+			want: Context{
+				AppBuild:     "1",
+				AppID:        "test",
+				ID:           androidOS,
+				OS:           androidOS,
+				DockerImage:  androidImage,
+				Volume:       vol,
+				CacheEnabled: true,
+				StripDebug:   true,
+				Package:      ".",
+				Keystore:     "/app/testdata/my.keystore",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx, err := makeAndroidContext(tt.args.flags, tt.args.args)
+
+			if tt.wantErr {
+				require.NotNil(t, err)
+				return
+			}
+			require.Nil(t, err)
+			assert.Equal(t, tt.want, ctx)
+		})
+	}
+}

internal/command/testdata/my.keystore
@@ -1,0 +1,1 @@
+test

commit e270d393f6a21267ea6f647799d421147999deba

Fix volume.JoinPathContainer should not depend on the OS
This commit fix a regression introduced in d949bc30.

internal/volume/volume.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 
@@ -103,7 +104,7 @@ func Mount(workDirHost string, cacheDirHost string) (Volume, error) {
 // JoinPathContainer joins any number of path elements into a single path,
 // separating them with the Container OS specific Separator.
 func JoinPathContainer(elem ...string) string {
-	return filepath.Clean(strings.Join(elem, "/"))
+	return path.Clean(strings.Join(elem, "/"))
 }
 
 // JoinPathHost joins any number of path elements into a single path,