d17ae6676eb22a0a238f6ef7d2a43bf6216b6063
Patrice Ferlet <metal3d@gmail.com>
2021-07-07 08:40:58 +0200

    Fixed: find "podman" when docker is not found

--- internal/command/docker.go
+++ internal/command/docker.go
@@ -28,8 +28,12 @@ const (
 // CheckRequirements checks if the docker binary is in PATH
 func CheckRequirements() error {
 	_, err := exec.LookPath("docker")
+	if err == nil {
+		return nil
+	}
+	_, err = exec.LookPath("podman")
 	if err != nil {
-		return fmt.Errorf("missed requirement: docker binary not found in PATH")
+		return fmt.Errorf("missed requirement: docker or podman binary not found in PATH")
 	}
 	return nil
 }
