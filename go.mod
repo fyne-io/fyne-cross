d2ec236886a7bc74df85f9b7cb8be844bdd159f5
Cedric BAIL <cedric@ddlm.me>
2022-08-01 17:35:26 -0600

    Move to archiver v3 in hope on compiling with go 1.14

--- go.mod
+++ go.mod
@@ -5,9 +5,14 @@ go 1.14
 require (
 	github.com/BurntSushi/toml v1.0.0
 	github.com/Kodeworks/golang-image-ico v0.0.0-20141118225523-73f0f4cfade9
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/aws/aws-sdk-go v1.43.31
-	github.com/mholt/archiver/v4 v4.0.0-alpha.6
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/mholt/archiver/v3 v3.5.1
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
 	github.com/stretchr/testify v1.7.0
+	github.com/ulikunitz/xz v0.5.10 // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9
 	k8s.io/api v0.23.5
 	k8s.io/apimachinery v0.23.5
--- internal/cloud/s3.go
+++ internal/cloud/s3.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
@@ -16,8 +17,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/klauspost/compress/zstd"
 
-	archiver "github.com/mholt/archiver/v4"
+	archiver "github.com/mholt/archiver/v3"
 )
 
 type AWSSession struct {
@@ -97,38 +99,74 @@ func (a *AWSSession) UploadFile(localFile string, s3FilePath string) error {
 }
 
 func (a *AWSSession) UploadCompressedDirectory(localDirectoy string, s3FilePath string) error {
-	files, err := archiver.FilesFromDisk(nil, map[string]string{
-		localDirectoy: "/",
-	})
-	if err != nil {
-		return err
-	}
+	in, out := io.Pipe()
 
 	extension := strings.ToLower(filepath.Ext(s3FilePath))
 
-	var compression archiver.Compression
+	var compression archiver.Writer
 
 	switch extension {
 	case ".xz":
-		compression = archiver.Xz{}
+		compression = archiver.NewTarXz()
+		err := compression.Create(out)
+		if err != nil {
+			return err
+		}
 	case ".zstd":
-		compression = archiver.Zstd{}
+		inZstd, outTar := io.Pipe()
+
+		compression = archiver.NewTar()
+		err := compression.Create(outTar)
+		if err != nil {
+			return err
+		}
+
+		enc, err := zstd.NewWriter(out)
+		if err != nil {
+			return err
+		}
+		defer enc.Close()
+
+		go func() {
+			io.Copy(enc, inZstd)
+		}()
 	default:
 		return fmt.Errorf("unknown extension for %v", s3FilePath)
 	}
 
-	format := archiver.CompressedArchive{
-		Compression: compression,
-		Archival:    archiver.Tar{},
-	}
-
-	in, out := io.Pipe()
-
 	errorChannel := make(chan error)
 
 	go func() {
-		err = format.Archive(context.Background(), out, files)
-		out.Close()
+		err := filepath.Walk(localDirectoy, func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			f, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			customName := strings.TrimPrefix(path, localDirectoy)
+			if len(customName) == 0 {
+				return fmt.Errorf("unexpected path: %v", path)
+			}
+			customName = filepath.ToSlash(customName)
+			if customName[0] != '/' {
+				customName = "/" + customName
+			}
+
+			return compression.Write(archiver.File{
+				FileInfo: archiver.FileInfo{
+					FileInfo:   info,
+					CustomName: customName,
+				},
+				ReadCloser: f,
+			})
+		})
+
+		compression.Close()
 
 		errorChannel <- err
 	}()
@@ -141,7 +179,7 @@ func (a *AWSSession) UploadCompressedDirectory(localDirectoy string, s3FilePath 
 	a.m.Unlock()
 	defer a.Cancel()
 
-	_, err = uploader.UploadWithContext(ctxt, &s3manager.UploadInput{
+	_, err := uploader.UploadWithContext(ctxt, &s3manager.UploadInput{
 		Bucket: aws.String(a.bucket),
 		Key:    aws.String(s3FilePath),
 
@@ -179,60 +217,60 @@ func (a *AWSSession) DownloadFile(s3FilePath string, localFile string) error {
 }
 
 func (a *AWSSession) DownloadCompressedDirectory(s3FilePath string, localRootDirectory string) error {
-	format := archiver.CompressedArchive{
-		Compression: archiver.Zstd{},
-		Archival:    archiver.Tar{},
-	}
-
 	in, out := io.Pipe()
+
+	extension := strings.ToLower(filepath.Ext(s3FilePath))
+
+	var compression archiver.Reader
+
+	switch extension {
+	case ".xz":
+		compression = archiver.NewTarXz()
+		err := compression.Open(in, 0)
+		if err != nil {
+			return err
+		}
+	case ".zstd":
+		inTar, outZstd := io.Pipe()
+
+		d, err := zstd.NewReader(in)
+		if err != nil {
+			return err
+		}
+		defer d.Close()
+
+		// Copy content...
+		_, err = io.Copy(outZstd, d)
+
+		compression = archiver.NewTar()
+		err = compression.Open(inTar, 0)
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("unknown extension for %v", s3FilePath)
+	}
 
 	errorChannel := make(chan error)
 
 	go func() {
-		err := format.Extract(context.Background(), in, nil, func(ctx context.Context, f archiver.File) error {
-			// Do not use strings.Split to split a path as it will generate empty string when given "//"
-			splitFn := func(c rune) bool {
-				return c == '/'
-			}
-			paths := strings.FieldsFunc(f.NameInArchive, splitFn)
-
-			if len(paths) == 0 {
-				return fmt.Errorf("incorrect path")
-			}
-
-			// Replace top directory in the archive with local path
-			paths[0] = localRootDirectory
-			localFile := filepath.Join(paths...)
-
-			if f.IsDir() {
-				if !Exists(localFile) {
-					log.Println("Creating directory:", localFile)
-					return os.Mkdir(localFile, f.Mode().Perm())
-				}
-				return nil
-			}
-
-			inFile, err := f.Open()
+		var err error
+		for {
+			f, err := compression.Read()
 			if err != nil {
-				return err
+				break
 			}
-			if inFile == nil {
-				// We are ignoring link for now
-				return nil
-			}
-			defer inFile.Close()
 
-			log.Println(f.NameInArchive, "->", localFile)
-			outFile, err := os.Create(localFile)
+			err = uncompressFile(localRootDirectory, f)
 			if err != nil {
-				return err
+				break
 			}
-			defer outFile.Close()
-			_, err = io.Copy(outFile, inFile)
+		}
 
-			return err
-		})
 		in.Close()
+		if err == io.EOF {
+			err = nil
+		}
 
 		errorChannel <- err
 	}()
@@ -266,6 +304,44 @@ func (a *AWSSession) Cancel() {
 
 	a.cancel()
 	a.cancel = func() {}
+}
+
+func uncompressFile(localRootDirectory string, f archiver.File) error {
+	// be sure to close f before moving on!!
+	defer f.Close()
+
+	// Do not use strings.Split to split a path as it will generate empty string when given "//"
+	splitFn := func(c rune) bool {
+		return c == '/'
+	}
+	paths := strings.FieldsFunc(f.Name(), splitFn)
+
+	if len(paths) == 0 {
+		return fmt.Errorf("incorrect path")
+	}
+
+	// Replace top directory in the archive with local path
+	paths[0] = localRootDirectory
+	localFile := filepath.Join(paths...)
+
+	if f.IsDir() {
+		if !Exists(localFile) {
+			log.Println("Creating directory:", localFile)
+			return os.Mkdir(localFile, f.Mode().Perm())
+		}
+		return nil
+	}
+
+	outFile, err := os.Create(localFile)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	log.Println(f.Name(), "->", localFile)
+	_, err = io.Copy(outFile, f)
+
+	return err
 }
 
 func (a *AWSSession) GetBucket() string {
