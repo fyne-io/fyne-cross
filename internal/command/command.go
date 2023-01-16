b751d944bec7a778e95055f285ffb942f843d66d
Cedric BAIL <cedric@ddlm.me>
2023-01-16 11:26:56 -0700

    Correct PATH lookup and joining.

--- internal/command/command.go
+++ internal/command/command.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/fyne-io/fyne-cross/internal/icon"
@@ -122,8 +123,8 @@ func cleanTargetDirs(ctx Context, image containerImage) error {
 func prepareIcon(ctx Context, image containerImage) error {
 	if !ctx.NoProjectUpload {
 		iconPath := ctx.Icon
-		if ctx.Icon[0] != os.PathSeparator {
-			iconPath = volume.JoinPathContainer(ctx.WorkDirHost(), ctx.Icon)
+		if !filepath.IsAbs(ctx.Icon) {
+			iconPath = volume.JoinPathHost(ctx.WorkDirHost(), ctx.Icon)
 		}
 
 		if _, err := os.Stat(iconPath); os.IsNotExist(err) {
@@ -132,7 +133,7 @@ func prepareIcon(ctx Context, image containerImage) error {
 			}
 
 			log.Infof("[!] Default icon not found at %q", ctx.Icon)
-			err = ioutil.WriteFile(volume.JoinPathContainer(ctx.WorkDirHost(), ctx.Icon), icon.FyneLogo, 0644)
+			err = ioutil.WriteFile(volume.JoinPathHost(ctx.WorkDirHost(), ctx.Icon), icon.FyneLogo, 0644)
 			if err != nil {
 				return fmt.Errorf("could not create the temporary icon: %s", err)
 			}
